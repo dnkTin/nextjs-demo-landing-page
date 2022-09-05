7a4bf09d106bde02d0449fc0ef4f308d2505a69a
Tin Dang <dnkt182@gmail.com>
2022-09-05 18:35:18 +0700

    first init

---
+++ comps/Navbar.js
@@ -0,0 +1,14 @@
+const Navbar = () => {
+  return (
+    <nav>
+      <div className="logo">
+        <h1>Ninja List</h1>
+      </div>
+      <a href="/home">Home</a>
+      <a href="/about">About</a>
+      <a href="/ninjas">Ninja Listing</a>
+    </nav>
+   );
+}
+ 
+export default Navbar;
---
+++ pages/ninjas/index.js
@@ -0,0 +1,7 @@
+const Ninjas = () => {
+  return ( 
+    <div>Default Ninjas</div>
+   );
+}
+ 
+export default Ninjas;
