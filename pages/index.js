9d1bde0f77738be3e21a141e8916b07613672696
Tin Dang <dnkt182@gmail.com>
2022-09-05 19:09:35 +0700

    finish layout for page

--- pages/index.js
+++ pages/index.js
@@ -7,12 +7,10 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <div>
-      <Navbar />
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id, quasi est itaque, cum aliquid eum similique illo quis debitis excepturi obcaecati vero rerum possimus consequuntur laborum minus aspernatur. Sit, et.</p>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id, quasi est itaque, cum aliquid eum similique illo quis debitis excepturi obcaecati vero rerum possimus consequuntur laborum minus aspernatur. Sit, et.</p>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id, quasi est itaque, cum aliquid eum similique illo quis debitis excepturi obcaecati vero rerum possimus consequuntur laborum minus aspernatur. Sit, et.</p>
       <Link href="/ninjas"><a>See Ninjas</a></Link>
-      <Footer />
     </div>
   )
 }
