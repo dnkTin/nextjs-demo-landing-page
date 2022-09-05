58c91359e8b9ea23e00931085e72e86b40f728d9
Tin Dang <dnkt182@gmail.com>
2022-09-05 18:39:12 +0700

    finish navbar and footer

---
+++ comps/Footer.js
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return ( 
+    <footer>
+      <div>
+        Copy rights 2021 - DNKT
+      </div>
+    </footer>
+   );
+}
+ 
+export default Footer;
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,18 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-
+import Navbar from '../comps/Navbar'
+import Footer from '../comps/Footer'
+import Link from 'next/link'
 export default function Home() {
   return (
     <div>
-      homepage
+      <Navbar />
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id, quasi est itaque, cum aliquid eum similique illo quis debitis excepturi obcaecati vero rerum possimus consequuntur laborum minus aspernatur. Sit, et.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id, quasi est itaque, cum aliquid eum similique illo quis debitis excepturi obcaecati vero rerum possimus consequuntur laborum minus aspernatur. Sit, et.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id, quasi est itaque, cum aliquid eum similique illo quis debitis excepturi obcaecati vero rerum possimus consequuntur laborum minus aspernatur. Sit, et.</p>
+      <Link href="/ninjas"><a>See Ninjas</a></Link>
+      <Footer />
     </div>
   )
 }
